66fb77bb13b5dae5bdb0d4fad5cf2da233c2effb
Sarah <sacred13_heart@yahoo.com>
2022-09-23 19:38:39 +0000

    update js

--- prompt1.js
+++ prompt1.js
@@ -4,7 +4,7 @@
 function getName(){
     let userName = prompt("what is your name?");
     if (userName == " " || userName == null ||userName == undefined){
-    alert ("Try again, where are you going?");
+    alert ("Try again! where are you going?");
     getName ();
     } else {
         document.write ("Hi " + userName + ", I love your name!");
