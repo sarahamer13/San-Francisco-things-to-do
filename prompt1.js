a19b2d886e2fbfe3a510d8b7daf3796eb68d55b1
Sarah <sacred13_heart@yahoo.com>
2022-09-26 07:09:35 +0000

    added loops

--- prompt1.js
+++ prompt1.js
@@ -12,3 +12,6 @@ function getName(){
 }
 getName ()
 
+
+
+
--- prompt2.js
+++ prompt2.js
@@ -2,12 +2,42 @@
 //document.write(usersemotion);
 
 
-function askTravel () {
-    let message = prompt ("Do you like to travel?");
-    if (message == "yes"){
-    document.write (" I love to travel too!");
-} else {
-    document.write (" You need a vacation");
+//function askTravel () {
+  // let message = prompt ("Do you like to travel?");
+   //if (message == "yes"){
+  // document.write (" I love to travel too!");
+//} else {
+   // document.write (" You need a vacation");
+//}
+//}
+//askTravel();
+
+    
+    function guessNumber () {
+        let response;
+        while (response != 2) {
+        response = prompt("How many people would you like to take to Ireland? Please pick a value from 1 to 5");
+        if (response !=2){
+            alert ("You are so close, try again!");
+        } else {
+            alert (" You are right!");
+        }
+    }
 }
-}
-askTravel();
+guessNumber ();
+
+        
+    
+    
+     
+
+
+    function showPic(){
+        let userNum = prompt("How many times have you seen San Francisco?");
+        for( let i = 0; i < userNum; i++){
+            document.write ("<img src= 'Getty_San-Francisco-CA-1.jpg' style='width:400px';/>")
+            }
+        }
+        
+
+
